e8200fb29cb8689d59ed8d669347bde32abea3ac
Unknown <carterasindinero@gmail.com>
2018-11-06 14:42:49 +0100

    Ej 7

---
+++ 07-CRUDPersonas/07-CRUDPersonas-DAL/Listados/ClsListadoPersonas.cs
@@ -0,0 +1,21 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_CRUDPersonas_DAL.Listados
+{
+    class ClsListadoPersonas
+    {
+        public List<ClsPersona> listadoCompletoPersonas()
+        {
+            List<ClsPersona> lista = new List<ClsPersona>();
+
+
+
+            return lista;
+        }
+    }
+}
---
+++ 07-CRUDPersonas/07-CRUDPersonas-Entidades/ClsDepartamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entidades
+{
+    public class ClsDepartamento
+    {
+        #region Constructor por defecto
+        public ClsDepartamento()
+        {
+
+        }
+        #endregion
+
+        #region Constructor con parámetros
+        public ClsDepartamento(int idDepartamento, string nombreDepartamento)
+        {
+            this.idDepartamento = idDepartamento;
+            this.nombreDepartamento = nombreDepartamento;
+        }
+        #endregion
+
+        #region "Atributos"
+        public int idDepartamento { get; set; }
+        public string nombreDepartamento { get; set; }
+        #endregion
+    }
+}
