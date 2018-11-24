595983f4aff0474d8c40668d80bf974bcf998fe1
Unknown <carterasindinero@gmail.com>
2018-11-24 17:14:14 +0100

    Añadido post, delete y put

--- 09 - ApiRESTPersonas/09 - ApiRESTPersonas_API/Controllers/PersonasController.cs
+++ 09 - ApiRESTPersonas/09 - ApiRESTPersonas_API/Controllers/PersonasController.cs
@@ -30,5 +30,38 @@ namespace _09___ApiRESTPersonas_API.Controllers
         {
             return ClsManejadoraPersona_BL.PersonaPorID_BL(id);
         }
+
+        /// <summary>
+        /// Verbo Post para agregar una persona a la base de datos
+        /// Devuelve true si se ha creado correctamente, false en caso contrario
+        /// </summary>
+        /// <param name="persona"></param>
+        /// <returns>bool</returns>
+        public bool Post([FromBody]ClsPersona persona)
+        {
+            return ClsManejadoraPersona_BL.CrearPersona_BL(persona) == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// Verbo Delete para borrar una persona de la base de datos dada una ID
+        /// Devuelve true si se ha borrado correctamente, false en caso contrario
+        /// </summary>
+        /// <param name="id">ID de la persona a borrar</param>
+        /// <returns>bool</returns>
+        public bool Delete(int id)
+        {
+            return ClsManejadoraPersona_BL.BorrarPorID_BL(id) == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// Verbo Put para actualizar una persona de la base de datos
+        /// Devuelve true si se ha actualizado correctamente, false en caso contrario
+        /// </summary>
+        /// <param name="persona">Persona con los datos actualizadas. La ID deber ser la misma</param>
+        /// <returns>bool</returns>
+        public bool Put([FromBody]ClsPersona persona)
+        {
+            return ClsManejadoraPersona_BL.ActualizarPersona_BL(persona) == 1 ? true : false;
+        }
     }
 }
