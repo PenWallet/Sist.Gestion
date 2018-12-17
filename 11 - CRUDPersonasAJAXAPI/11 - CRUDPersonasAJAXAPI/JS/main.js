5324f5285cbbe3c306ec4863edb4324e70f15504
Unknown <carterasindinero@gmail.com>
2018-12-17 23:39:38 +0100

    Fila de editar hecha

--- 11 - CRUDPersonasAJAXAPI/11 - CRUDPersonasAJAXAPI/JS/main.js
+++ 11 - CRUDPersonasAJAXAPI/11 - CRUDPersonasAJAXAPI/JS/main.js
@@ -46,6 +46,7 @@ function cargarListado(data)
 
         //También el elemento table data (td)
         var td = document.createElement("td");
+        td.setAttribute("name", "fila" + data[i].idPersona)
 
         //Y el checkbox para hacer el input
         var checkBox = document.createElement("input");
@@ -63,16 +64,19 @@ function cargarListado(data)
         //Recorremos todas las filas de información e insertamos
         for (var prop in data[0])
         {
-            td = document.createElement("td");
-            var textoCelda = document.createTextNode(data[i][prop]);
-            td.appendChild(textoCelda);
-            if (prop == "idPersona")
-                td.style.display = "none";
-            tr.appendChild(td);
+            if (prop != "idPersona") {
+                td = document.createElement("td");
+                td.setAttribute("name", "fila" + data[i].idPersona)
+                var textoCelda = document.createTextNode(prop != "fechaNac" ? data[i][prop] : data[i][prop].split("T")[0]);
+                td.appendChild(textoCelda);
+                tr.appendChild(td);
+            }
         }
+
         //Agregamos el botón Edit, que viene a partir de este HTML:
         //<p title="Edit"><button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-pencil"></span></button></p>
         td = document.createElement("td");
+        td.setAttribute("name", "fila" + data[i].idPersona)
         var p = document.createElement("p");
         p.setAttribute("title", "Editar");
         var buttonEdit = document.createElement("button");
@@ -91,7 +95,39 @@ function cargarListado(data)
 }
 
 function editPersona() {
-    alert(this.value);
+    var fila = "fila" + this.value;
+    var columnas = document.getElementsByName(fila);
+    var arrayDatos = new Array();
+
+    columnas[0].innerHTML = "";
+    var idDepartamento = columnas[1].innerHTML; arrayDatos.push(idDepartamento);
+    var nombre = columnas[2].innerHTML; arrayDatos.push(nombre);
+    var apellidos = columnas[3].innerHTML; arrayDatos.push(apellidos);
+    var fechaNac = columnas[4].innerHTML; fechaNac = fechaNac.split("T")[0]; arrayDatos.push(fechaNac);
+    var direccion = columnas[5].innerHTML; arrayDatos.push(direccion);
+    var telefono = columnas[6].innerHTML; arrayDatos.push(telefono);
+    columnas[7].innerHTML = "";
+
+    //Ponemos los inputs
+    columnas[1].innerHTML = "";
+    var input = document.createElement("input");
+    input.setAttribute("type", "number");
+    input.setAttribute("name", "txtDatos");
+    input.setAttribute("class", "form-control");
+    input.setAttribute("value", idDepartamento);
+    columnas[1].appendChild(input);
+
+    for (var i = 2; i < 7; i++) {
+        columnas[i].innerHTML = "";
+        var input = document.createElement("input");
+        input.setAttribute("type", i != 4 ? "text" : "date");
+        input.setAttribute("name", "txtDatos");
+        input.setAttribute("class", "form-control");
+        input.setAttribute("value", arrayDatos[i-1])
+        columnas[i].appendChild(input);
+    }
+
+    disableAllEditButtons();
 }
 
 function borrarPersonasClick() {
@@ -154,7 +190,7 @@ function anadirPersona() {
     for (var i = 2; i < 7; i++) {
         celda = row.insertCell(i);
         var input = document.createElement("input");
-        input.setAttribute("type", "text");
+        input.setAttribute("type", i != 4 ? "text":"date");
         input.setAttribute("name", "txtDatos");
         input.setAttribute("class", "form-control");
         celda.appendChild(input);
@@ -170,35 +206,37 @@ function anadirPersona() {
 }
 
 function aceptarPersona() {
-    var arrayDatos = document.getElementsByName("txtDatos");
-    var idDepartamento = arrayDatos[0].value;
-    var nombre = arrayDatos[1].value;
-    var apellidos = arrayDatos[2].value;
-    var fechaNac = arrayDatos[3].value;
-    var direccion = arrayDatos[4].value;
-    var telefono = arrayDatos[5].value;
-    var persona = new Persona(0, nombre, apellidos, fechaNac, direccion, telefono, idDepartamento);
-    var json = JSON.stringify(persona);
+    if (checkPersonInfo()) {
+        var arrayDatos = document.getElementsByName("txtDatos");
+        var idDepartamento = arrayDatos[0].value;
+        var nombre = arrayDatos[1].value;
+        var apellidos = arrayDatos[2].value;
+        var fechaNac = arrayDatos[3].value;
+        var direccion = arrayDatos[4].value;
+        var telefono = arrayDatos[5].value;
+        var persona = new Persona(0, nombre, apellidos, fechaNac, direccion, telefono, idDepartamento);
+        var json = JSON.stringify(persona);
 
-    //Hacemos la llamada POST a la API
-    var llamada = new XMLHttpRequest();
-    llamada.open("POST", "https://apipennypersonas.azurewebsites.net/api/Personas/", true);
-    llamada.setRequestHeader("Content-Type", "application/json");
-    
-    llamada.onreadystatechange = function () {
-        if (llamada.readyState == 4 && llamada.status == 200) {
-            alert("¡Persona creada correctamente!");
+        //Hacemos la llamada POST a la API
+        var llamada = new XMLHttpRequest();
+        llamada.open("POST", "https://apipennypersonas.azurewebsites.net/api/Personas/", true);
+        llamada.setRequestHeader("Content-Type", "application/json");
 
-            //CargarListado
-            recargarListado();
-            
-            hideBtnAceptar();
-            hideBtnRechazar();
-            showBtnAnadir();
-        }
-    };
+        llamada.onreadystatechange = function () {
+            if (llamada.readyState == 4 && llamada.status == 200) {
+                alert("¡Persona creada correctamente!");
 
-    llamada.send(json);
+                //CargarListado
+                recargarListado();
+
+                hideBtnAceptar();
+                hideBtnRechazar();
+                showBtnAnadir();
+            }
+        };
+
+        llamada.send(json);
+    }
 }
 
 function rechazarPersona() {
@@ -208,8 +246,47 @@ function rechazarPersona() {
     showBtnAnadir();
 }
 
+function checkPersonInfo() {
+    var arrayDatos = document.getElementsByName("txtDatos");
+    var idDepartamento = arrayDatos[0].value;
+    var nombre = arrayDatos[1].value;
+    var apellidos = arrayDatos[2].value;
+    var fechaNac = arrayDatos[3].value;
+    var direccion = arrayDatos[4].value;
+    var telefono = arrayDatos[5].value;
+    var esValido = true;
 
+    if (idDepartamento == 0) {
+        alert("¡La ID del departamento no puede estar vacía ni ser 0!");
+        esValido = false;
+    }
+    else if (nombre == "") {
+        alert("¡El nombre no puede estar vacío!");
+        esValido = false;
+    }
+    else if (apellidos == "") {
+        alert("¡Los apellidos no pueden estar vacíos!");
+        esValido = false;
+    }
+    else if (fechaNac == "") {
+        alert("¡La fecha de nacimiento no puede estar vacía!");
+        esValido = false;
+    }
+    else if (new Date(fechaNac) > new Date()) {
+        alert("¡Fecha inválida! No creo que vayas a nacer en el futuro...");
+        esValido = false;
+    }
+    else if (direccion == "") {
+        alert("¡La dirección no puede estar vacía!");
+        esValido = false;
+    }
+    else if (telefono == "") {
+        alert("¡El teléfono no puede estar vacío!");
+        esValido = false;
+    }
 
+    return esValido;
+}
 
 
 
@@ -293,4 +370,18 @@ function hideBtnRechazar() {
 function showBtnRechazar() {
     var botonRechazar = document.getElementById("btnRechazar");
     botonRechazar.style.display = "";
+}
+
+function disableAllEditButtons() {
+    var botones = document.getElementsByName("btnEdit");
+    for (var i = 0; i < botones.length; i++) {
+        botones[i].disabled = true;
+    }
+}
+
+function enableAllEditButtons() {
+    var botones = document.getElementsByName("btnEdit");
+    for (var i = 0; i < botones.length; i++) {
+        botones[i].disabled = false;
+    }
 }
